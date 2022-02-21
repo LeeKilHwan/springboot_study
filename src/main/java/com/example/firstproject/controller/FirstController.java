77ec9aca4cbf5b913301c00654caac30eba1d6e2
LeeKilHwan <dlrlfghks94@gmail.com>
2022-02-21 16:04:04 +0900

    뷰 템플릿과 MVC패턴

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    //GetMapping을 통해서 접속할 URL주소를 넣어주어야 반환값으로 접속이 가능하다
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "KilHwan");
+        return "greetings";  // templates/greetings.mustache -> 브라우저로 전송!
+    }
+}
