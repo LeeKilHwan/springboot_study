db728fcc40a5ecfef3cefea8d8adcd5abca6a043
LeeKilHwan <dlrlfghks94@gmail.com>
2022-02-23 17:21:09 +0900

    데이터 목록조회 (데이터 베이스 저장 되어있는 전체 목록 조회)

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,8 +3,12 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 // CrudRepository를 상속받아 CrudRepository가 가지고 있는 기능들을 사용할 수 있도록 해준다.
 // CrudRepository<관리 대상 entity, 관리 대상 entity의 대표값의 타입> 으로 상속해준다.
 //  >> Article(관리 대상 entity)을 CURD하는 추가코드 구현없이 사용할 수 있게 된다.
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
