44b7789cb0e58956a382050eda0cf9abb020ef51
LeeKilHwan <dlrlfghks94@gmail.com>
2022-02-23 16:53:06 +0900

    데이터 조회하기 (입력한 데이터를 데이터베이스에 저장하고 불러와 조회하기)

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 @Entity  // >> DB가 해당 객체를 인식 가능하게 된다.
 
 @AllArgsConstructor
+@NoArgsConstructor  // 디폴트 생성자를 추가해주는 어노테이션
 @ToString
 public class Article {
     @Id // 대표값을 지정! (주민등록번호 같은 것)  // 필드의 값이 같을 때 그것을 구별하기 위해 사용
